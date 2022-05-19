e397f4365744cd37c4b47dada7a31f03c8d9ff1b
ddddavid <ddddavid@macbook.host>
2022-05-20 04:51:21 +0800

    first commit

---
+++ HTMLGenerator.py
@@ -0,0 +1,290 @@
+class Generator:
+    def __init__(self, dates, urls, titles, descriptions, col=3, today_size=70):
+        self.dates = dates
+        self.urls = urls
+        self.titles = titles
+        self.descriptions = descriptions
+        self.col = col
+        self.today_size = today_size
+        self.mainpage = None
+        self.subpages = {}
+        self.small_req = "&rf=LaDigue_UHD.jpg&pid=hp&w=WIDTH&h=HEIGHT&rs=1&c=4"
+        self.head = [
+                "<!DOCTYPE html>",
+                "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"\" xml:lang=\"\">",
+                "<head>",
+                "\t<meta charset=\"utf-8\"/>",
+                "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\"/>",
+                "\t<title>Bing Wallpaper</title>",
+                "\t<style type=\"text/css\">",
+                "\t\tcode{white-space: pre-wrap;}",
+                "\t\tspan.smallcaps{font-variant: small-caps;}",
+                "\t\tspan.underline{text-decoration: underline;}",
+                "\t\tdiv.column{display: inline-block; vertical-align: top; width: 50%;}",
+                "\t</style>",
+                "</head>",
+                "<a name=\"top\"></a>",
+                "<br>",
+                "<hr>",
+                "<br>",
+                "<h1 align=\"center\">Bing Wallpaper  Gallery</h1>",
+                "<br>",
+                "<hr>",
+                "<br>",
+                "",
+                "<body>"
+        ]
+        self.tail = [
+            "\t\t<p class=\"bottom-space\"></p>",
+            "",
+            "\t\t<hr>",
+            "\t\t<br>",
+            "\t\t<nav>",
+            "\t\t<h4 align=\"center\">",
+            "\t\t\t<a href=\"\#top\">Back To Top</a>",
+            "\t\t\t&nbsp;",
+            "\t\t\t<a href=\"http://file.home.ddddavid.cn:83/BingWallpaper\">Plain File List</a>",
+            "\t\t\t&nbsp;",
+            "\t\t\t<a href=\"https://cn.bing.com\">See Bing Here</a>",
+            "\t\t</h4>",
+            "\t\t<!-- <h4 align=\"center\">",
+            "\t\t\t<a href=\"https://github.com/niumoo/bing-wallpaper\">Source From</a>",
+            "\t\t</h4> -->",
+            "\t\t<br>",
+            "\t\t<h4 align=\"center\">",
+            "\t\t\t<a target=\"_blank\" href=\"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011202001496\" style=\"display:inline-block;text-decoration:none;height:20px;line-height:20px;\">",
+            "\t\t\t\t<p style=\"float:left;height:20px;line-height:20px;margin: auto; color:\#939393;\">",
+            "\t\t\t\t\t粤公网安备 44011202001496号",
+            "\t\t\t\t</p>",
+            "\t\t\t</a>",
+            "\t\t</h4>",
+            "\t\t",
+            "\t\t</nav>",
+            "\t\t",
+            "\t\t<!-- <hr> -->",
+            "\t\t<br>",
+            "",
+            "\t</body>",
+            "",
+            "",
+            "\t<style>",
+            "\t\t*{",
+            "\t\t\tmargin: 0;",
+            "\t\t\tpadding: 0;",
+            "\t\t}",
+            "\t\t.row-container{",
+            "\t\t\twidth: 100%;",
+            "\t\t\tdisplay: flex;",
+            "\t\t\tdisplay: -webkit-flex;",
+            "\t\t\tmargin-top: 20px;",
+            "\t\t\t/* justify-content: space-around; */",
+            "\t\t\tjustify-content: center;",
+            "\t\t\t/* vertical-align: middle; */",
+            "\t\t\t/* margin: 20px auto; */",
+            "\t\t}",
+            "\t\t.item-container{",
+            f"\t\t\twidth: {(100-10)/self.col}%;",
+            "\t\t\theight: auto;",
+            "\t\t\tvertical-align: middle;",
+            "\t\t\tmargin: 1 auto;",
+            "\t\t}",
+            "\t\t.today-item-container{",
+            f"\t\t\twidth: {self.today_size}%;",
+            "\t\t\theight: auto;",
+            "\t\t\tvertical-align: middle;",
+            "\t\t\tmargin: 1 auto;",
+            "\t\t\tmargin-bottom: 5;",
+            "\t\t}",
+            "",
+            "\t\t.img-container{",
+            "\t\t\twidth: 100%;",
+            "\t\t\theight:auto;",
+            "\t\t}",
+            "\t\timg{",
+            "\t\t\twidth: 100%;",
+            "\t\t\theight: auto;",
+            "\t\t}",
+            "\t\t.img-title{",
+            "\t\t\tpadding: 10px 0;",
+            "\t\t\ttext-align: center;",
+            "\t\t\tfont-size: 16;",
+            "\t\t\tfont-weight: normal;",
+            "\t\t}",
+            "\t\t.bottom-space {",
+            "\t\t\tmargin-bottom: 1cm;",
+            "\t\t}",
+            "\t</style>",
+            "",
+            "",
+            "\t</html>"
+        ]
+        self.head = "\n".join(self.head)
+        self.tail = "\n".join(self.tail)
+        
+
+    def genItem(self, date, url, lazy=False, resolution=(384, 216)):
+        full_date = f"20{date[:2]}-{date[2:4]}-{date[4:]}"
+        # date = date[2:]
+        # small_url = url.replace('UHD', '1280x720')
+        small_url = f"{url}{self.small_req}"
+        small_url = small_url.replace('WIDTH', str(resolution[0]))
+        small_url = small_url.replace('HEIGHT', str(resolution[1]))
+        subpage = f"page-{date}.html"
+        if lazy:
+            loading = "loading=\"lazy\""
+        else:
+            loading = ""
+        code = ""
+        code += "\t<div class=\"item-container\">\n"
+        code += "\t\t<div class=\"img-container\">\n"
+        code += f"\t\t<a href={subpage} target=\"_blank\">"
+        code += f"\t\t\t<img src={small_url} alt=\"bing picture\" {loading}>\n"
+        code += "\t\t</a>"
+        code += "\t\t</div>\n"
+        code += "\t\t<h5 class=\"img-title\">\n"
+        # code += f"\t\t\t<a href=\"{url}\">{full_date}</a>\n"
+        code += f"\t\t\t{full_date} &nbsp;\n"
+        code += f"\t\t\t<a href={subpage} target=\"_blank\">View Detail</a> &nbsp;\n"
+        code += f"\t\t\t<a href=\"BW-{date}.jpg\" download=\"\">Download 4K</a>\n"
+        code += "\t\t</h5>\n"
+        code += "\t</div>\n"
+        return code
+
+
+    def genToday(self):
+        description = self.descriptions[0]
+        date = self.dates[0][2:]
+        url = self.urls[0]
+        # print(url)
+        small_url = url.replace('UHD', '1920x1080')
+        code = ""
+        code += "\t<div class=\"today-item-container\">\n"
+        code += "\t\t<div class=\"img-container\">\n"
+        code += f"\t\t\t<a href=\"page-{date}.html\" target=\"_blank\">"
+        code += f"\t\t\t\t<img src={small_url} alt=\"\">\n"
+        code += "\t\t\t</a>"
+        code += "\t\t</div>\n"
+        code += "\t\t<h3 class=\"img-title\">\n"
+        code += f"\t\t\t<b>Today: </b>{description}\n"
+        code += "\t\t</h3>\n"
+        code += "\t</div>\n"
+        return code
+        
+
+    def genRow(self, items):
+        code = ""
+        code += "<div class=\"row-container\">\n"
+        for item in items:
+            code += item
+            code += "&nbsp;"
+        code = code[:-6]
+        code += "</div>"
+        code += "\n"
+        return code
+
+
+    def genMainPage(self):
+        code = ""
+        code += self.head
+        code += self.genRow([self.genToday()])
+        
+        num = len(self.dates)
+        col = self.col
+        row_num = num // col
+        for i in range(row_num):
+            items = []
+            if i > 10:
+                lazy = True
+            else:
+                lazy = False
+            for j in range(col):
+                date = self.dates[i*col+j][2:]
+                url = self.urls[i*col+j]
+                items.append(self.genItem(date, url, lazy=lazy))
+            code += self.genRow(items)
+        if(num%col != 0):
+            items = []
+            for j in range(num%3):
+                date = self.dates[row_num*3+j][2:]
+                url = self.urls[row_num*3+j]
+                items.append(self.genItem(date, url, lazy=True))
+            code += self.genRow(items)
+        code += "\n\n"
+        code += self.tail
+            
+        self.mainpage = code 
+        return code 
+        
+
+
+    def genSubpage(self, date, url, title, description):
+        date = date[2:]
+        code = [
+            "<!DOCTYPE html>",
+            "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"\" xml:lang=\"\">",
+            "<head>",
+            "\t<meta charset=\"utf-8\">",
+            "\t<title>BW</title>",
+            "</head>",
+            "",
+            "<br><br>",
+            "<body>",
+            "\t<div  style=\"",
+            "\t\t\twidth: 100%;",
+            "\t\t\tdisplay: flex;",
+            "\t\t\tdisplay: -webkit-flex;",
+            "\t\t\tmargin-top: 20px;",
+            "\t\t\tjustify-content: center;\"",
+            "\t>",
+            "",
+            "\t<div  style=\"",
+            "\t\t\twidth: 90%;",
+            "\t\t\theight: auto;",
+            "\t\t\tvertical-align: middle;",
+            "\t\t\tmargin: 1 auto\"",
+            "\t>",
+            "",
+            "\t\t<div style=\"width: 100%;height:auto;\">",
+            f"\t\t\t<img src={url} ",
+            "\t\t\t\talt=\"\" width=\"100%\" height=\"auto\"",
+            "\t\t\t>",
+            "\t\t</div>",
+            "",
+            "\t\t<div style=\">",
+            "\t\t\t\tpadding: 10px 0;",
+            "\t\t\t\ttext-align: center;",
+            "\t\t\t\tfont-size: 16;",
+            "\t\t\t\tfont-weight: normal;\"",
+            "\t\t>",
+            "\t\t\t<h3 align=\"center\">",
+            f"\t\t\t\t<b>{title}:&nbsp;</b>",
+            f"\t\t\t\t{description} &nbsp;",
+            f"\t\t\t\t<a href=\"BW-{date}.jpg\" download=\"\">",
+            "\t\t\t\t\tDownload",
+            "\t\t\t\t</a>",
+            "\t\t\t</h3>",
+            "\t\t</div>",
+            "\t</div>",
+            "\t</div>",
+            "</body>",
+            "</html>"
+        ]
+        code = "\n".join(code)
+        
+        self.subpages[f"{date}"] = code
+        return code
+    
+    
+    def generateAll(self):
+        self.genMainPage()
+        num = len(self.dates)
+        for i in range(num):
+            self.genSubpage(
+                self.dates[i],
+                self.urls[i],
+                self.titles[i],
+                self.descriptions[i]
+            )
+        return num
+    
+    
---
+++ fetch.py
@@ -0,0 +1,218 @@
+
+
+import os
+import argparse
+import requests as re
+import wget
+import joblib as jbl
+from HTMLGenerator import Generator as HG
+
+
+
+data_base = "./source_list.jbl"
+img_dir = "./wallpaper/images"
+html_dir = "./wallpaper/html"
+cache_dir = "./cache"
+backup_dir = "./backup"
+# re_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10"
+re_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1614319565639&pid=hp&FORM=BEHPTB&uhd=1"
+url_base = "https://cn.bing.com"
+img_prefix = "BW"
+
+
+def add2Backup(src):
+    with open(f'{backup_dir}/{data_base}.bak', 'a+') as file:
+        keys = list(src.keys())
+        for i in range(len(src[keys[0]])):
+            line = ''
+            for k in keys:
+                line += f'{src[k][i]}\t'
+            line = line[:-1]
+            file.write(line)
+
+
+ap = argparse.ArgumentParser(description='Bing Wallpaper Fetcher')
+ap.add_argument('--image-only', action='store_true', help='只下载图片')
+ap.add_argument('--html-only', action='store_true', help='只生成网页')
+ap.add_argument('--keep-cache', action='store_true', help='不删除临时文件')
+ap.add_argument('--no-image', action='store_true', help='不下载图片')
+ap.add_argument('--no-html', action='store_true', help='不生成网页')
+ap.add_argument('--column-number', type=int, default=3, help='网页中一行的图片数')
+ap.add_argument('--no-history', action='store_true', help='Caution! Stop loading source list.')
+# ap.add_argument('--add-item', type=str, nargs='+', help='手动添加项目（json格式）')
+# ap.add_argument('--sort-item', action='store_true', help='对item排序')
+# TODO: 添加排序功能
+
+args = ap.parse_args()
+if args.image_only and (not args.html_only):
+    image_on = True
+    html_on = False
+elif args.html_only and (not args.image_only):
+    html_on = True
+    image_on = False
+elif (not args.image_only) and (not args.html_only):
+    if (not args.no_image) and (not args.no_html):
+        image_on = html_on = True
+    elif args.no_image and args.no_html:
+        image_on = html_on = False
+    elif args.no_image:
+        html_on = True
+        image_on = False
+    else:
+        html_on = False
+        image_on = False
+else:
+    print("Conflicting parameters. Check again.")
+    exit(1)
+
+
+if os.path.exists(img_dir):
+    ...
+else:
+    os.makedirs(img_dir)
+if os.path.exists(html_dir):
+    ...
+else:
+    os.makedirs(html_dir)
+if os.path.exists(cache_dir):
+    ...
+else:
+    os.makedirs(cache_dir)
+if os.path.exists(backup_dir):
+    if os.path.exists(f'{backup_dir}/html'):
+        ...
+    else:
+        os.makedirs(f'{backup_dir}/html')
+else:
+    os.makedirs(backup_dir)
+
+
+## read data base
+# {
+#   'date':[date], 'url':[url],
+#   'description':[description], 'title':[title]
+# }
+# newer one rank higher
+if os.path.exists(data_base) and (not args.no_history):
+    src = jbl.load(data_base) 
+else:
+    src = {'date':[], 'url':[], 'description':[], 'title':[]}
+for k in src.keys():
+    src[k].reverse()
+# newer one at behind
+
+
+## find updates
+response = re.get(re_url).json()
+
+img_jss = response['images']
+
+
+
+n_dates = [] # [YYYY-MM-DD]
+n_urls = [] 
+n_descriptions = []
+n_titles = []
+
+for img_js in img_jss:
+    n_dates.append(img_js['enddate'])
+    n_urls.append(f"{url_base}{img_js['urlbase']}_UHD.jpg")
+    n_descriptions.append(img_js['copyright'])
+    n_titles.append(img_js['title'])
+
+n_dates.reverse()
+n_urls.reverse()
+n_descriptions.reverse()
+n_titles.reverse()
+
+
+# TODO: Iteration needed to be generalize
+for date, url, description, title in zip(
+    n_dates, n_urls, n_descriptions, n_titles
+):
+    if date not in src['date']:
+        src['date'].append(date)
+        src['url'].append(url)
+        src['description'].append(description)
+        src['title'].append(title)
+    else:
+        ...
+
+tmp = src.copy()
+for k in src.keys():
+    tmp[k].reverse()
+    src[k] = []
+# newer one at the front
+
+
+add2Backup(tmp) # backup old src
+# prune items older than one year
+y_now = int(tmp['date'][0][2:4])
+for date, url, description, title in \
+    zip(tmp['date'], tmp['url'], tmp['description'], tmp['title']):
+    if int(date[2:4]) - y_now <= 1:
+        src['date'].append(date)
+        src['url'].append(url)
+        src['description'].append(description)
+        src['title'].append(title)
+    else:
+        ...
+
+jbl.dump(src, data_base)
+
+
+
+## download images
+if image_on:
+    downloaded_imgs = os.listdir(img_dir)
+
+    for date, url in zip(src['date'], src['url']):
+        if f'{img_prefix}-{date[2:]}.jpg' not in downloaded_imgs:
+            wget.download(url, out=f'{cache_dir}/img_cache')
+            """
+            TODO: needed to add error detection
+            """
+            os.rename(f'{cache_dir}/img_cache', f'{img_dir}/{img_prefix}-{date[2:]}.jpg')
+            print(f'-> {img_prefix}-{date[2:]} downloaded.')
+        else:
+            ...
+else:
+    print('-> image downloading skipped')
+
+
+## generate html files 
+if html_on:
+    hg = HG(
+        src['date'], src['url'], src['title'], src['description'],
+        col=args.column_number
+    )
+
+    num = hg.generateAll()
+    with open(f'{cache_dir}/index.html', 'w') as file:
+        file.write(hg.mainpage)
+    for key in hg.subpages.keys():
+        with open(f'{cache_dir}/page-{key}.html', 'w') as file:
+            file.write(hg.subpages[key])
+            
+    if num > 0:
+        # os.system(f'rm -f {backup_dir}/html/*.html')
+        os.system(f'mv {html_dir}/*.html {backup_dir}/html/') # BUG: backup don't update
+        os.system(f'cp {cache_dir}/index.html {html_dir}/')
+        os.system(f'cp {cache_dir}/page-*.html {html_dir}/')
+                    
+
+    print(f'->{html_dir}/index.html generated \n ->{num} {html_dir}/page-*.html has been generated.')
+else:
+    print('-> html generation skipped')
+
+
+if not args.keep_cache:
+    os.system(f'rm -f {cache_dir}/img_cache*')
+    os.system(f'rm -f {cache_dir}/*.html')
+else:
+    ...
+    
+
+exit(0)
+  
+
